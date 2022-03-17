7c2e13baadbe597790e67501389f1859b3fbb9e1
cristhian1107 <cristhian.cjaa@gmail.com>
2022-03-17 17:34:11 -0500

    [Add] Function Send Mail

---
+++ Utilities-CSharp/Utilities/EMails.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+
+
+namespace Utilities
+{
+    public static class EMails
+    {
+        public static string SendMail(string To, string CC, string Bcc)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress("repuestos@autrisa.com");
+                message.To.Add(new MailAddress(To));
+                message.CC.Add(new MailAddress(CC));
+                message.Bcc.Add(new MailAddress(Bcc));
+                message.Subject = "Test";
+                message.IsBodyHtml = true; 
+                message.Body = "<h3> Prueba de correo enviado fuera de red <h3>";
+                smtp.Port = 587;
+                smtp.Host = "SMTP.Office365.com";
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("repuestos@autrisa.com", "R123456+");
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+                return ("Email send succes");
+            }
+            catch (Exception ex) 
+            {
+                return (ex.Message);
+            }
+        }
+    }
+}
---
+++ Utilities-CSharp/Utilities/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string parameters = Console.ReadLine();
+            string[] tokens = parameters.Split(' ');
+            if (tokens.Length != 0)
+            {
+                switch (tokens[0])
+                {
+                    case "SendMail":
+                        string result = EMails.SendMail(tokens[1], tokens[2], tokens[3]);
+                        Console.WriteLine("{0} {1}", result, tokens[1]);
+                        break;
+                    default :
+                        Console.WriteLine("Function not exits");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
