9f5a198162932a240f5101c56e655b64ccfa70b5
cristhian1107 <cristhian.cjaa@gmail.com>
2022-03-17 17:58:58 -0500

    [Add] Comments

--- Utilities-CSharp/Utilities/EMails.cs
+++ Utilities-CSharp/Utilities/EMails.cs
@@ -12,13 +12,20 @@ namespace Utilities
 {
     public static class EMails
     {
+        /// <summary>
+        /// Simple Funcion Send Email
+        /// </summary>
+        /// <param name="To">Email to</param>
+        /// <param name="CC">Email in copy</param>
+        /// <param name="Bcc">Email in hidden copy</param>
+        /// <returns>Messahe status de send</returns>
         public static string SendMail(string To, string CC, string Bcc)
         {
             try
             {
                 MailMessage message = new MailMessage();
                 SmtpClient smtp = new SmtpClient();
-                message.From = new MailAddress("repuestos@autrisa.com");
+                message.From = new MailAddress("micorreo@dominio.com");
                 message.To.Add(new MailAddress(To));
                 message.CC.Add(new MailAddress(CC));
                 message.Bcc.Add(new MailAddress(Bcc));
@@ -26,10 +33,11 @@ namespace Utilities
                 message.IsBodyHtml = true; 
                 message.Body = "<h3> Prueba de correo enviado fuera de red <h3>";
                 smtp.Port = 587;
+                // info! smtp.gmail.com for gmail
                 smtp.Host = "SMTP.Office365.com";
                 smtp.EnableSsl = true;
                 smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new NetworkCredential("repuestos@autrisa.com", "R123456+");
+                smtp.Credentials = new NetworkCredential("micorreo@dominio.com", "micontraseña");
                 smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
                 smtp.Send(message);
                 return ("Email send succes");
